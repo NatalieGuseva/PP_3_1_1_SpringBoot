900dc6379017515b9a7f0fa868952978c6947c0c
NatalieGuseva <gusberd81@gmail.com>
2024-12-16 00:24:38 +0500

    Task executed

---
+++ src/main/java/com/springboot/pp_3_1_1_springboot/dao/UserDaoImpl.java
@@ -0,0 +1,40 @@
+package com.springboot.pp_3_1_1_springboot.dao;
+
+import org.springframework.stereotype.Repository;
+import com.springboot.pp_3_1_1_springboot.model.User;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    public User getByIdUser(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    public void update(Long id, User updateUser) {
+        User userToBeUpdate = getByIdUser(id);
+        userToBeUpdate.setName(updateUser.getName());
+        userToBeUpdate.setEmail(updateUser.getEmail());
+    }
+
+    public void delete(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+}
---
+++ src/main/java/com/springboot/pp_3_1_1_springboot/service/UserService.java
@@ -0,0 +1,18 @@
+package com.springboot.pp_3_1_1_springboot.service;
+
+
+import com.springboot.pp_3_1_1_springboot.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAllUsers();
+
+    public User getByIdUser(Long id);
+
+    public void save(User user);
+
+    public void update(Long id, User updateUser);
+
+    public void delete(Long id);
+}
