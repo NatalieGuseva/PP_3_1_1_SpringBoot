cce2388e28f00ec647314e53fa6ae59afe67fd04
NatalieGuseva <gusberd81@gmail.com>
2024-12-17 20:31:21 +0500

    Task executed

--- src/main/java/com/springboot/pp_3_1_1_springboot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.pp_3_1_1_springboot.repository;
-
-import com.springboot.pp_3_1_1_springboot.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
